0cbed609a8dd5ab159eee79eebda2c7b0cbdeebb
TRamos5 <travis.ramos5@gmail.com>
2018-08-19 15:07:35 -0500

    chat feature

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 let data = require('./data/data.json');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('view engine', 'ejs');
 app.set('appData', data);
@@ -70,16 +72,24 @@ app.get('/puppies', (req, res) =>{
     })
 });
 
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+  });
+  
+
 
 app.use(require('./routes/index'));
 app.use(require('./routes/puppies'));
 app.use(require('./routes/feedback'));
 app.use(require('./routes/api'));
+app.use(require('./routes/chat'));
 
 
 
 
 
-app.listen(9000, () =>{
+http.listen(9000, () =>{
     console.log('listening port 9000');
 });
